8cdcab246f7d2ba54dd9a2681eed540c549197ed
Alex <best1972@list.ru>
2023-04-13 16:13:01 +0300

    made task 3

--- Tasks.java
+++ Tasks.java
@@ -93,7 +93,7 @@
 //         for (int i = ages.size() - 1; i >= 0; i--) {
 //             for (Entry<String, Integer> entry : counter.entrySet()) {
 //                 if (entry.getValue() > 1 && entry.getValue() == ages.get(i)) {
-//                     sb.append(entry.getKey() + ":" + Integer.toString(entry.getValue()));
+//                     sb.append(entry.getKey() + " - " + Integer.toString(entry.getValue()));
 //                     sb.append(System.lineSeparator());
 //                     counter.remove(entry.getKey());
 //                     break;
@@ -101,6 +101,51 @@
 //             }
 //         }
 //         System.out.println(sb.toString());
+//     }
+// }
+
+// Задача 3. На шахматной доске расставить 8 ферзей так, чтобы они не били друг друга. И вывести Доску. 
+// Пример вывода доски 8x8
+// 0x000000
+// 0000x000
+// 00x00000
+ 
+// public class Tasks {
+//     public static void main(String[] args) {
+//         int[] queens = new int[8];
+//         findPosition(0, queens);
+//         for (int row = 0; row < 8; row++) {
+//             for (int col = 0; col < 8; col++) {
+//                 if (queens[row] == col) {
+//                     System.out.print("X");
+//                 } else {
+//                     System.out.print("0");
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+
+//     private static boolean findPosition(int row, int[] queens) {
+//         if (row == 8) {
+//             return true;
+//         }
+//         for (int col = 0; col < 8; col++) {
+//             boolean isSafe = true;
+//             for (int i = 0; i < row; i++) {             
+//                 if (queens[i] == col || queens[i] == col - row + i || queens[i] == col + row - i) {
+//                     isSafe = false;
+//                     break;
+//                 }
+//             }
+//             if (isSafe) {
+//                 queens[row] = col; 
+//                 if (findPosition(row + 1, queens)) { 
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
 //     }
 // }
 
